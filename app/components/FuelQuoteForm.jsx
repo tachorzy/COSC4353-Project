45961d3906e4cf2bd0ed6e32ca286733bbb6bf65
Tariq Achor Zyad <tachorzyad@gmail.com>
2023-02-23 18:25:04 -0600

    Resized input boxes and changed background color to slate

--- app/components/FuelQuoteForm.jsx
+++ app/components/FuelQuoteForm.jsx
@@ -21,16 +21,32 @@ const FuelQuoteForm = () => {
                 </div>
                 <div className={FuelQuoteStyle.inputContainer}>
                     <label for="gallons-requested" className={FuelQuoteStyle.label}>Gallons:</label>
-                    <input className={FuelQuoteStyle.inputBox} placeholder={"Gallons"} name="gallons-requested"/>
+                    <input className={FuelQuoteStyle.smallInputBox} placeholder={"Gallons"} name="gallons-requested"/>
                 </div>
 
                 <div className={FuelQuoteStyle.inputContainer}>
-                    <label for="delivery-address" className={FuelQuoteStyle.label}>Address:</label>
-                    <input className={FuelQuoteStyle.inputBox} placeholder={"Delivery Address"} name="delivery-address"/>
+                    <label for="delivery-address" className={FuelQuoteStyle.label}>Address 1:</label>
+                    <input className={FuelQuoteStyle.addressInputBox} placeholder={"Delivery Address"} name="delivery-address"/>
+                </div>
+            
+                <div className={FuelQuoteStyle.inputContainer}>
+                    <label for="delivery-address" className={FuelQuoteStyle.label}>Address 2: {`(Optional)`}</label>
+                    <input className={FuelQuoteStyle.addressInputBox} placeholder={"Delivery Address"} name="delivery-address"/>
+                </div>
 
                 <div className={FuelQuoteStyle.inputContainer}>
                     <label for="delivery-date" className={FuelQuoteStyle.label}>Delivery Date:</label>
-                    <input className={FuelQuoteStyle.inputBox} placeholder={"Delivery Date"} name="delivery-date"/>
+                    <input className={FuelQuoteStyle.deliveryInputBox} placeholder={"Delivery Date"} name="delivery-date"/>
+                </div>
+                <div className={FuelQuoteStyle.splitContainer}>
+                    <div className={FuelQuoteStyle.inputContainer}>
+                        <label for="zip-code" className={FuelQuoteStyle.label}>Zip Code:</label>
+                        <input className={FuelQuoteStyle.deliveryInputBox} placeholder={"Zip Code"} name="zip-cpde"/>
+                    </div>
+                    <div className={FuelQuoteStyle.inputContainer}>
+                        <label for="delivery-date" className={FuelQuoteStyle.label}>Delivery Date:</label>
+                        <input className={FuelQuoteStyle.deliveryInputBox} placeholder={"Delivery Date"} name="delivery-date"/>
+                    </div>
                 </div>
                 <buttton className={FuelQuoteStyle.calculateButton}>Calculate Your Fuel Quote!</buttton>
             </form>  
