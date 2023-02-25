71b29d62fac5b587d08b4ad81961ef8e81ea5f53
Druasoul <wwwilliam.le@gmail.com>
2023-02-25 00:21:53 -0600

    updated

--- app/pages/FuelQuote.jsx
+++ app/pages/FuelQuote.jsx
@@ -4,13 +4,20 @@ import FuelQuoteForm from '../components/FuelQuoteForm.jsx'
 import { Inter } from '@next/font/google'
 import { Combo, Roboto, Rubik } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import localFont from '@next/font/local'
 import FuelQuoteStyle from '../styles/FuelQuoteStyle.module.css'
+import FuelQuoteHistory from '../components/FuelQuoteHistory.jsx'
 
 const inter = Inter({ subsets: ['latin'] })
 
 const roboto = Roboto({ 
   subsets: ['latin'], 
   weight: '400' 
+})
+
+const satoshi = localFont({
+  src: '../fonts/Satoshi-Regular.otf',
+  weight: '200'
 })
 
 export default function FuelQuote() {
@@ -23,7 +30,10 @@ export default function FuelQuote() {
         <link rel="icon" href="/favicon.ico"/>
       </Head>
       <main className={styles.main}>
-        <FuelQuoteForm></FuelQuoteForm>
+        <div className={satoshi.className}>
+          <FuelQuoteForm/>
+          <h1 className="text-9xl">Calculate your Fuel Quotes</h1>
+        </div>
       </main>
     </>
   )
