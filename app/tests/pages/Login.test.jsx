63a0eafbf29887bdda0a01d8b407156e12d0c86e
Tariq Achor Zyad <tachorzyad@gmail.com>
2023-03-12 03:17:01 -0500

    Added unit test for user validation on Login page

--- app/tests/pages/Login.test.jsx
+++ app/tests/pages/Login.test.jsx
@@ -22,7 +22,6 @@ describe('Login component', () => {
 
     it('Disable the submit button if either field is left empty', () => {
         render(<Login/>);
-
         const submitButton = screen.getByRole('button', { name: "Sign in"});
         
         expect(submitButton).toBeDisabled();
@@ -36,8 +35,44 @@ describe('Login component', () => {
 
         expect(emailInput).toHaveValue("testemail@gamil.com");
         expect(passwordInput).toHaveValue("testpassword")
-        
     })
+
+    it('Input should not contain any invalid characters e.g. whitespace', () => {
+        render(<Login/>);
+
+        const emailInput = screen.getByPlaceholderText("Email address");
+        const passwordInput = screen.getByPlaceholderText("Password");
+        const submitButton = screen.getByRole('button', { name: "Sign in"});
+
+        fireEvent.change(emailInput, { target: { value: "testemail @gmail.com" }});
+        fireEvent.change(passwordInput, { target: { value: "test password"}});
+        fireEvent.click(submitButton);
+
+        expect(emailInput).toBe("testemail@gamil.com");
+        expect(passwordInput).toBe("testpassword");
+    })
+    
+    it('Must validate and find the correct user by an email and password', () => {
+        const users = new Map([
+            ["test@gmail.com", "test"], 
+            ["tachorzyad@gmail.com", "fakepassword"], 
+            ["randomuser@yahoo.com", "fakepassword2"], 
+            ["jest@gmail.com", "totallyArealPassword444"], 
+            ["gabenewell@valvesoftware.com", "test"], 
+            ["YetAnotherFakeUser@gmail.com", "testtestest"]
+        ])
+
+        const userEmail = "test@gmail.com";
+        const userPassword = "test";
+
+        const userExists = users.has(userEmail) && users.get(userEmail) === userPassword;
+        
+        if(!userExists)
+            sendMessageMock('Invalid email or password!')
+
+        expect(userExists).toBe(true);
+    })
+
 })
 
 
