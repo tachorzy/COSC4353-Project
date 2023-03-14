b5111dc662a17fb40032faa44b9594455f5be33a
Druasoul <wwwilliam.le@gmail.com>
2023-03-13 23:07:44 -0500

    issue with styling

--- app/components/FuelQuoteForm.jsx
+++ app/components/FuelQuoteForm.jsx
@@ -9,23 +9,24 @@ const roboto = Roboto({
     subsets: ['latin'], 
     weight: '400' 
 })
-  
-
+var UsaStates = require('usa-states').UsaStates;
+var usStates = new UsaStates();
+var statesAbrev = usStates.arrayOf('abbreviations');
 const FuelQuoteForm = () => {
     return(
         <div className={roboto.className}>
             <form className={FuelQuoteStyle.container}>
                 <div className={FuelQuoteStyle.inputContainer}>
-                    <input className={FuelQuoteStyle.standardInputBox} placeholder={"First Name"} name="first-name"/>
+                    <input className={FuelQuoteStyle.standardInputBox} placeholder={"First Name"} name="first-name" required/>
                 </div>  
                 <div className={FuelQuoteStyle.inputContainer}>
-                    <input className={FuelQuoteStyle.standardInputBox} placeholder={"Last Name"} name="last-name"/>
+                    <input className={FuelQuoteStyle.standardInputBox} placeholder={"Last Name"} name="last-name" required/>
                 </div>  
                 <div className={FuelQuoteStyle.inputContainer}>
-                    <input className={FuelQuoteStyle.standardInputBox} placeholder={"E-mail address"} name="email"/>
+                    <input className={FuelQuoteStyle.standardInputBox} placeholder={"E-mail address"} name="email" required/>
                 </div>  
                 <div className={FuelQuoteStyle.inputContainer}>
-                    <input className={FuelQuoteStyle.standardInputBox} placeholder={"Address 1"} name="delivery-address"/>
+                    <input className={FuelQuoteStyle.standardInputBox} placeholder={"Address 1"} name="delivery-address"required/>
                 </div>
             
                 <div className={FuelQuoteStyle.inputContainer}>
@@ -33,13 +34,17 @@ const FuelQuoteForm = () => {
                 </div>
                 
                 <div className={FuelQuoteStyle.splitContainer}>
-                        <input className={FuelQuoteStyle.smallInputBox} placeholder={"Zip Code"} name="zip-cpde"/>
-                        <input className={FuelQuoteStyle.smallInputBox} placeholder={"State"} name="state"/>
+                        <input  className={FuelQuoteStyle.smallInputBox} placeholder={"Zip Code"} name="zip-code" min ="5" max = "9" required />
+                        <select className={FuelQuoteStyle.smallInputBox} placeholder={"State"} name="state" id = "state"/>
+                            {statesAbrev.map((state) => (
+                            <option key={state} value={state}>
+                            {state}
+                            </option>
+                            ))}
                 </div>
                 <div className={FuelQuoteStyle.inputContainer}>
                     <div className={FuelQuoteStyle.logoInputContainer}>
-                        <input className={FuelQuoteStyle.standardInputBox} placeholder={"Delivery Date"} name="delivery-date"/>
-                        <Image src='/calendar.svg' width={28} height={28} className={FuelQuoteStyle.calendarLogo}></Image>
+                        <input type = "date" className={FuelQuoteStyle.standardInputBox} placeholder={"Delivery Date"} name="delivery-date"/>
                     </div>
 
                 
