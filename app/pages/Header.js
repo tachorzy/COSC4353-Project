675d290d7d5d834de7dc4d3f65ac555ee0a262e2
Tina Chinehkesh <123219005+Tinachinehkesh@users.noreply.github.com>
2023-04-09 13:20:07 -0500

    Updated navbar

--- app/pages/Header.js
+++ app/pages/Header.js
@@ -5,38 +5,56 @@ import { useSession, signOut } from "next-auth/react";
 
 const Header = () => {
     const {data} = useSession();
-    console.log(data);
     
   return (
     <nav className="navbar navbar-light bg-light row justify-content-center sticky-top">
+      
       <div className="container">
-        <div className="col-3 p-0">
-          <a className="navbar-brand" style={{ marginLeft: "20px" }} href="#">
-            Authentication
-          </a>
-        </div>
 
-        <div className="col-3 mt-3 mt-md-0 text-center d-flex flex-row">
-          {data? (
-            <>
-              <span style={{ marginRight: "15px" }}>
-                Hi, {data.user.personalDetails[0].FirstName}
-              </span>
+        <div className="col-3 mt-3 mb-3 text-center">
+            
+            {data? (
+              <div className="flex gap-10 justify-center justify-around">
 
-              <span style={{ cursor: "pointer" }} onClick={() => signOut()}>
-                {" "}
-                Logout
-              </span>
-            </>
-          ) : (
-            <span style={{ marginRight: "15px" }}>
-              {" "}
-              <Link className="nav-link" href="/Login">
+                
+                <span className="font-semibold">
+                  Hi, {data.user.personalDetails[0].FirstName}
+                </span>
+                
+                <Link className="nav-link" href="/FuelQuote">
+                  Quotes
+                </Link>
+
+                <Link className="nav-link" href="/Profile">
+                  Profile
+                </Link>
+
+                <Link className="nav-link" href="/History">
+                  History
+                </Link>
+
+                <Link className="nav-link" onClick={() => signOut()} href="/login">
+                  Logout
+                </Link>
+              
+              </div>
+            
+            ) : (
+
+            <div className="flex gap-10 justify-center justify-around">
+              <Link className="nav-link font-semibold" href="/Login">
                 Login
               </Link>
-            </span>
+
+              <Link className="nav-link font-semibold" href="/Register">
+                Register
+              </Link>
+              
+            </div>
+
           )}
         </div>
+
       </div>
     </nav>
   );
